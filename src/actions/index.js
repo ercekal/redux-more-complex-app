4e3d15c4c742c43da2b6c67327b1ec68149b65a5
ercekal <ercekalabalikoglu@gmail.com>
2016-12-13 16:20:16 +0000

    chapter 82 however I couldn't see the post in network tab

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=123dasd'
+const ROOT_URL = "http://reduxblog.herokuapp.com/api";
+const API_KEY = "?key=123dasd"
 
 export function fetchPosts () {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
